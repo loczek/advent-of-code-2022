6546bc62c579d3b1b63ee4fb06a2aa841f303a1a
loczek <30776250+loczek@users.noreply.github.com>
2022-12-02 09:10:57 +0100

    day 2

---
+++ 02/b.ts
@@ -0,0 +1,49 @@
+import { getLines } from "../utils.ts";
+
+async function main() {
+  const lines = await getLines(2);
+
+  const winMap = {
+    A: "Y",
+    B: "Z",
+    C: "X",
+  } as const;
+  const drawMap = {
+    A: "X",
+    B: "Y",
+    C: "Z",
+  } as const;
+  const loseMap = {
+    A: "Z",
+    B: "X",
+    C: "Y",
+  } as const;
+  const pointsMap = {
+    X: 1,
+    Y: 2,
+    Z: 3,
+  };
+
+  let res = 0;
+
+  for (const line of lines) {
+    let [him, me] = line.split(" ") as ["A" | "B" | "C", "X" | "Y" | "Z"];
+    if (me === "Z") {
+      me = winMap[him];
+    } else if (me === "Y") {
+      me = drawMap[him];
+    } else if (me === "X") {
+      me = loseMap[him];
+    }
+
+    res += pointsMap[me];
+    if (winMap[him] === me) {
+      res += 6;
+    } else if (drawMap[him] === me) {
+      res += 3;
+    }
+  }
+  console.log(res);
+}
+
+main();
