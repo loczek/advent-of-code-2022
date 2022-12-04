24c13b344f4b59ddd2da08f53afdb0f67cae347d
loczek <30776250+loczek@users.noreply.github.com>
2022-12-04 22:54:13 +0100

    day 4

---
+++ 04/a.ts
@@ -0,0 +1,23 @@
+import { getLines } from "../utils.ts";
+
+async function main() {
+  const lines = await getLines(4);
+
+  let count = 0;
+  for (let i = 0; i < lines.length; i++) {
+    const [first, second] = lines[i].split(",");
+    const [firstStart, firstEnd] = first.split("-");
+    const [secondStart, secondEnd] = second.split("-");
+
+    if (
+      (+firstStart <= +secondStart && +firstEnd >= +secondEnd) ||
+      (+firstStart >= +secondStart && +firstEnd <= +secondEnd)
+    ) {
+      count++;
+    }
+  }
+
+  console.log(count);
+}
+
+main();
